b6a0cfd126c469f7923a1cced9a1b7b590740a06
Marianna-Dalterio <mariannadalterio00@gmail.com>
2025-10-16 17:11:45 +0200

    add contacts links

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,5 +1,29 @@
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
 export default function Contacts() {
     return (
-        <h5>Here you can get in touch with us</h5>
+        <div className="container">
+            <h1>Contattaci</h1>
+            <div className="row">
+                <div className="col px-4">
+                    <a class="icon-link d-block mb-3" href="#">
+                        <FaFacebook className="me-2" />
+                        Facebook
+                    </a>
+                    <a class="icon-link d-block mb-3" href="#">
+                        <FaInstagram className="me-2" />
+                        Instagram
+                    </a>
+                    <a class="icon-link d-block" href="#">
+                        <FaTwitter className="me-2" />
+                        Twitter
+                    </a>
+                </div>
+            </div>
+
+        </div>
+
     )
 };
